dfa23148d5b83ec7c9b36ed30ff0dd6f6cfd00d4
Raul Victor <31050006+Raul772@users.noreply.github.com>
2022-05-29 14:31:27 -0300

    Create Project005

---
+++ Projeto005/script.js
@@ -0,0 +1,6 @@
+const hoursText = document.getElementsByClassName("hour") 
+const minutesText = document.getElementsByClassName("minutes") 
+const secondsText = document.getElementsByClassName("seconds")
+
+
+
