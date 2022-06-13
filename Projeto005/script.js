f7285d7c6d42dc2cc7dfa57527d301e582170289
Raul Victor <31050006+Raul772@users.noreply.github.com>
2022-06-13 15:47:59 -0300

    Add dynamic time update

--- Projeto005/script.js
+++ Projeto005/script.js
@@ -1,6 +1,22 @@
-const hoursText = document.getElementsByClassName("hour") 
-const minutesText = document.getElementsByClassName("minutes") 
-const secondsText = document.getElementsByClassName("seconds")
+let hours = document.querySelector(".hour");
+let minutes = document.querySelector(".minutes");
+let seconds = document.querySelector(".seconds");
+let period = document.querySelector(".period");
 
+const clock = setInterval(function updateTime(){
+    let date = new Date();
+    let newHours = date.getHours();
+    let newMinutes = date.getMinutes().toString().padStart(2, "0");
+    let newSeconds = date.getSeconds().toString().padStart(2, "0");
 
+    
+    minutes.textContent = newMinutes;
+    seconds.textContent = newSeconds;
 
+    if(newHours > 12){
+        period.textContent = "PM";
+        hours.textContent = (newHours - 12).toString().padStart(2, "0");
+    }else{
+        hours.textContent = (newHours).toString().padStart(2, "0");
+    }
+});
