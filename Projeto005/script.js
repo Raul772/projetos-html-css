4951f7600bea5ec3be50753d3794c06eb445f79f
Raul Victor <31050006+Raul772@users.noreply.github.com>
2022-07-24 01:04:18 -0300

    Add better responsivity
    Add new JS Code for more animation, and CSS for responsivity

--- Projeto005/script.js
+++ Projeto005/script.js
@@ -2,22 +2,37 @@ let hours = document.querySelector(".hour");
 let minutes = document.querySelector(".minutes");
 let seconds = document.querySelector(".seconds");
 let period = document.querySelector(".period");
+let blinking_dots = document.getElementsByClassName("blink-dots");
+
+let date;
+let newHours;
+let newMinutes;
+let newSeconds;
 
 const clock = setInterval(function updateTime(){
-    let date = new Date();
-    let newHours = date.getHours();
-    let newMinutes = date.getMinutes().toString().padStart(2, "0");
-    let newSeconds = date.getSeconds().toString().padStart(2, "0");
+    date = new Date();
+    newHours = date.getHours();
+    newMinutes = date.getMinutes().toString().padStart(2, "0");
+    newSeconds = date.getSeconds().toString().padStart(2, "0");
 
     
-    minutes.textContent = newMinutes;
-    seconds.textContent = newSeconds;
+    minutes.innerHTML = newMinutes;
+    seconds.innerHTML = newSeconds;
 
     if(newHours >= 12){
-        period.textContent = "PM";
-        hours.textContent = (newHours - 12).toString().padStart(2, "0");
+        period.innerHTML = "PM";
+        hours.innerHTML = (newHours - 12).toString().padStart(2, "0");
     }else{
-        period.textContent = "AM";
-        hours.textContent = (newHours).toString().padStart(2, "0");
+        period.innerHTML = "AM";
+        hours.innerHTML = (newHours).toString().padStart(2, "0");
     }
-});
+
+    if(blinking_dots[0].style.visibility == "visible") { 
+        blinking_dots[0].style.visibility = "hidden";
+        blinking_dots[1].style.visibility = "hidden";
+    } else {
+        blinking_dots[0].style.visibility = "visible";
+        blinking_dots[1].style.visibility = "visible";
+    }
+
+}, 1000);
