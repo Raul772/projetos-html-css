0f4a0f8414b6de54b59fe779d2f7a132efd4ea4a
Raul Victor <31050006+Raul772@users.noreply.github.com>
2022-09-14 13:51:03 -0300

    Move Projects 003, 005 and 006 to their own repository

--- Projeto005/responsive.js
+++
@@ -1,6 +0,0 @@
-setInterval(function(){ 
-    
-    if(window.innerWidth <= 600){
-        document.querySelector(".text").innerHTML = '<span class="hour">00</span><br><span class="blink-dots">:</span><br><span class="minutes">00</span><br><span class="blink-dots">:</span><br><span class="seconds">00</span><br><span class="period">AM</span>'
-    }
-});
